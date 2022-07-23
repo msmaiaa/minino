d272c62bae4cda98d8eb0cf41a14f5133dc25946
Matheus Maia <roxx77344@gmail.com>
2022-07-23 00:07:17 -0300

    crate: build cli application with options to list, remove and add fields on the path

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,49 @@
+use clap::{Args, Parser, Subcommand};
 pub use minino::Path;
-fn main() {}
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    List,
+    Push(PushArg),
+    Remove(RemoveArg),
+}
+
+#[derive(Args, Debug)]
+struct PushArg {
+    #[clap(value_parser)]
+    path: String,
+}
+
+#[derive(Args, Debug)]
+struct RemoveArg {
+    #[clap(value_parser)]
+    pattern: String,
+}
+
+fn main() {
+    use Command::*;
+    let cli = Cli::parse();
+    let path = Path::new();
+    match &cli.command {
+        List => {
+            let values = path.get_value_as_vec().unwrap();
+            for i in values.into_iter() {
+                println!("{:?}", i);
+            }
+        }
+        Push(data) => {
+            path.push_value(data.path.clone()).unwrap();
+        }
+        Remove(data) => {
+            path.remove_value(data.pattern.clone()).unwrap();
+        }
+    }
+}
